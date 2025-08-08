d42b4903c0c76ece08cb45afbbe7065dae7ffd07
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-08 13:45:10 -0700

    Add web app App.js

---
+++ frontend/web-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import logo from './assets/images/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Blackroad Search</h1>
+        <p>Welcome to BlackStream web app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
